56267569a1ecbdc4c7765f31032f2019d7ac218d
Vadzim <vvoitkus@mail.ru>
2019-04-03 18:02:03 +0300

    Update timer2.js

--- lesson 16/timer2.js
+++ lesson 16/timer2.js
@@ -1,11 +1,12 @@
 var startP = document.getElementById("startPause");
-clockFace = document.getElementById("clockFace");
-saveBtn = document.createElement("button");
-reset = document.createElement("button");
-container = document.getElementsByClassName('container')[0];
-
-time = 0;
-running = 0;
+    clockFace = document.getElementById("clockFace");
+    saveBtn = document.createElement("button");
+    reset = document.createElement("button");
+    container = document.getElementsByClassName('container')[0];
+    controls = document.getElementById('controls');
+    time = 0;
+    running = 0;
+var timerSet;
 
 //создание кнопки Запустить-возобновить
 startP.addEventListener('click', startPause, false);
@@ -24,7 +25,7 @@ function startPause() {
     running = 0;
     startP.innerHTML = "Возобновить";
     startP.style.backgroundColor = "bisque";
-  };
+  }
 }
 
 //создание кнопки Сбросить
@@ -33,14 +34,19 @@ reset.addEventListener('click', resetValue, false);
 function resetValue() {
   running = 0;
   time = 0;
+
   startP.innerHTML = "Запустить";
+  clearTimeout(timerSet);
   clockFace.innerHTML = "00:00:00";
+
   var elementsOfEndTime = document.querySelectorAll('.results');
-  console.log(elementsOfEndTime);
-  for (var i = 0; i < elementsOfEndTime.length; i++) {
-    elementsOfEndTime[i].parentNode.removeChild(elementsOfEndTime[i]);
-    count = 1;
-  };
+    for (var i = 0; i < elementsOfEndTime.length; i++) {
+      elementsOfEndTime[i].parentNode.removeChild(elementsOfEndTime[i]);
+      count = 1;
+    }
+  
+    controls.removeChild(saveBtn);
+    controls.removeChild(reset); 
 }
 
 //создание кнопки Сохранить отметку
@@ -58,20 +64,28 @@ function saveValue() {
 
 //создание секундомера
 function timer() {
-  if (running == 1) {
-    setTimeout(function () {
+  if (running == 1)
+    timerSet = setTimeout(function () {
+      if (running == 1) {
       time++;
-      var mins = Math.floor(time / 10 / 60);
-      var secs = Math.floor(time / 10 % 60);
-      var tenths = time % 10;
+      var mins = Math.floor(time / 100 / 60);
+      var secs = Math.floor(time / 100 % 60);
+      var tenths = time % 100;
       if (mins < 10) {
         mins = "0" + mins;
       }
       if (secs < 10) {
         secs = "0" + secs;
       }
-      clockFace.innerHTML = mins + ":" + secs + ":" + tenths + "0";
+      if (tenths < 10) {
+        tenths = "0" + tenths;
+      }
+      if (time >= 3.6e6) {
+        alert('Максимальное время вышло!');
+        return;
+      }
+      clockFace.innerHTML = mins + ":" + secs + ":" + tenths;
       timer();
-    }, 100);
-  };
+    }
+  }, 10);
 }
